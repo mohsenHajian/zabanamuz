1b9e42e9b89d5bce20ce1d20040f5a1411ce846e
mohsen3451 <m.hajian3451@gmail.com>
2024-06-28 14:22:39 +0330

    rollBack

--- src/main.js
+++ src/main.js
@@ -1,29 +1,29 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-import { Icon } from "@iconify/vue";
+import { Icon } from '@iconify/vue';
 
 // repositories
-import repositories from "../plugins/repositories";
+import repositories from '../plugins/repositories';
 // import axiosMessage from '../plugins/axiosMessage';
-import globalProperties from "../plugins/globalProperties";
+import globalProperties from '../plugins/globalProperties';
 
 // directives
-import clickOutside from "../directives/click-outside";
+import clickOutside from '../directives/click-outside';
 
 // mixin
-import { globalMixin } from "../mixin/globalMixin";
+import { globalMixin } from '../mixin/globalMixin';
 
-import BlockViewer from "@/components/BlockViewer.vue";
+import BlockViewer from '@/components/BlockViewer.vue';
 
-import "@/assets/styles.scss";
+import '@/assets/styles.scss';
 
 // custom components
-import LoadingSpinner from "../components/Global/LoadingSpinner.vue";
+import LoadingSpinner from '../components/Global/LoadingSpinner.vue';
 
 // store
-import store from "../store/index";
+import store from '../store/index';
 
 const app = createApp(App);
 // app.config.globalProperties.$store = store;
@@ -34,7 +34,7 @@ app.use(store);
 app.mixin(globalMixin);
 app.use(router);
 
-app.component("Icon", Icon);
+app.component('Icon', Icon);
 
 // app.use(PrimeVue, { ripple: true });
 // app.use(PrimeVue, {
@@ -44,15 +44,15 @@ app.component("Icon", Icon);
 // app.use(DialogService);
 // app.use(ConfirmationService);
 
-app.directive("click-outside", clickOutside);
+app.directive('click-outside', clickOutside);
 // app.directive('tooltip', Tooltip);
 // app.directive('badge', BadgeDirective);
 // app.directive('ripple', Ripple);
 // app.directive('styleclass', StyleClass);
 
-app.component("BlockViewer", BlockViewer);
+app.component('BlockViewer', BlockViewer);
 
 // custom components
-app.component("LoadingSpinner", LoadingSpinner);
+app.component('LoadingSpinner', LoadingSpinner);
 
-app.mount("#app");
+app.mount('#app');
